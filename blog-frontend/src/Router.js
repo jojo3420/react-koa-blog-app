4eaf3dd56472343b57b7d43bbc0031e349fa1c53
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-17 19:45:27 +0900

    [frontend] 라우터 개선 -  code split 적용  - add @loadable/component  - code splitting

--- blog-frontend/src/Router.js
+++ blog-frontend/src/Router.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import LoginPage from 'pages/LoginPage';
-import PostDetailPage from 'pages/PostDetailPage';
-import PostListPage from 'pages/PostListPage';
-import PostWritePage from 'pages/PostWritePage';
-import SignUpPage from 'pages/SignUpPage';
+import loadable from '@loadable/component';
+import { Spin } from 'antd';
+
+// import LoginPage from 'pages/LoginPage';
+// import PostDetailPage from 'pages/PostDetailPage';
+// import PostListPage from 'pages/PostListPage';
+// import PostWritePage from 'pages/PostWritePage';
+// import SignUpPage from 'pages/SignUpPage';
+
+
+const LoginPage = loadable(() => import('pages/LoginPage'), {
+  fallback: <Spin tip="loading.." />,
+});
+
+const PostDetailPage = loadable(() => import('pages/PostDetailPage'), {
+  fallback: <Spin tip="loading.." />,
+});
+
+const PostListPage = loadable(() => import('pages/PostListPage'), {
+  fallback: <Spin tip="loading.." />,
+});
+
+const PostWritePage = loadable(() => import('pages/PostWritePage'), {
+  fallback: <Spin tip="loading.." />,
+});
+
+const SignUpPage = loadable(() => import('pages/SignUpPage'), {
+  fallback: <Spin tip="loading.." />,
+});
+
 
 function Router () {
   return (
